45dcc1b1ffc8e776f8ca598389493f30112be1e5
BGarciaD <borjagarciadelgado9@gmail.com>
2022-01-30 23:05:11 +0000

    Add spreadsheetlight dependency. Add method CreateXLSXFile which creates a new data table, creates a column for the name of the files, inserts the file names into the data table, import the data table into an xlsx file and save it

--- spreadsheet-creator/Program.cs
+++ spreadsheet-creator/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.IO;
+using SpreadsheetLight;
 
 namespace spreadsheet_creator
 {
@@ -8,7 +9,6 @@ namespace spreadsheet_creator
         static void Main(string[] args)
         {
             string tmpPath = Environment.GetFolderPath(Environment.SpecialFolder.MyMusic);
-            Console.WriteLine(tmpPath);
             GetAllFiles(tmpPath);
             Console.WriteLine();
             Console.WriteLine("Press any key to finish");
@@ -22,13 +22,34 @@ namespace spreadsheet_creator
                 string[] pathExistingFiles = Directory.GetFiles(path);
                 if (pathExistingFiles.Length > 0)
                 {
-                    Console.WriteLine(pathExistingFiles.Length + " files found");
+                    CreateXLSXFile(path, pathExistingFiles);
                 }
                 else
                 {
                     Console.WriteLine("No files found in this directory");
                 }
             }
+        }
+
+        public static void CreateXLSXFile(string path, string[] filesArray)
+        {
+            SLDocument oSLDocument = new SLDocument();
+            System.Data.DataTable dt = new System.Data.DataTable();
+            Console.WriteLine("Creating data table...");
+            // Columns - Headers
+            dt.Columns.Add("File Name", typeof(string));
+            // Rows - Data
+            foreach (string test in filesArray)
+            {
+                dt.Rows.Add(Path.GetFileName(test));
+            }
+            Console.WriteLine("Importing data table to spreadsheet...");
+            oSLDocument.ImportDataTable(1, 1, dt, true);
+            Console.WriteLine("Insert the name of the spreadsheet file: ");
+            string userInput_fileName = Console.ReadLine();
+            Console.WriteLine("Saving file...");
+            oSLDocument.SaveAs(path + "\\" + userInput_fileName + ".xlsx");
+            Console.WriteLine("Done");
         }
     }
 }
