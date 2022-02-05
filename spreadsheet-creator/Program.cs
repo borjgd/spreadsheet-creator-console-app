d9be64022d7461240eb24ae230e9d71655db275c
BGarciaD <borjagarciadelgado9@gmail.com>
2022-02-05 02:12:21 +0000

    Modify "CreateXLSXFile" method in order to show a process status when reading the files of the target folder resolving the issue #3

--- spreadsheet-creator/Program.cs
+++ spreadsheet-creator/Program.cs
@@ -39,16 +39,34 @@ namespace spreadsheet_creator
 
         public static void CreateXLSXFile(string path, string[] filesArray)
         {
+            int filesArrayLength = filesArray.Length;
+            int count = 0;
             SLDocument oSLDocument = new SLDocument();
             System.Data.DataTable dt = new System.Data.DataTable();
             Console.WriteLine("Creating data table...");
             // Columns - Headers
             dt.Columns.Add("File Name", typeof(string));
+            // Hide cursor
+            Console.CursorVisible = false;
             // Rows - Data
             foreach (string test in filesArray)
             {
+                if (count > 0)
+                {
+                    int currentLineCursor = Console.CursorTop;
+                    Console.SetCursorPosition(0, currentLineCursor);
+                    Console.Write(new string(' ', Console.WindowWidth));
+                    Console.SetCursorPosition(0, currentLineCursor);
+                }
+                count++;
+                Console.Write(string.Format("{0}/{1}", count, filesArrayLength));
+                // Add file to rows
                 dt.Rows.Add(Path.GetFileName(test));
             }
+            // Show Cursor
+            Console.CursorVisible = true;
+            Console.WriteLine();
+            // Import datatable to spreadsheet
             Console.WriteLine("Importing data table to spreadsheet...");
             oSLDocument.ImportDataTable(1, 1, dt, true);
             Console.WriteLine("Insert the name of the spreadsheet file: ");
